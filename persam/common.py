d3f0f3362762183a263a8f4421cdc710932b32f7
Andrew Healey <doolie.healey@gmail.com>
2023-07-11 01:04:58 -0400

    Fix another linear_combo error

--- persam/common.py
+++ persam/common.py
@@ -149,7 +149,7 @@ def predict_mask_refined(
         # Weighted sum of three-scale masks
         logits_high = logits_high * logit_weights[...,None,None]
         logit_high = logits_high.sum(0)
-        mask = (logit_high > 0)
+        mask = (logit_high > 0).cpu().detach().numpy()
 
         logit_weights_np = logit_weights.detach().cpu().numpy()
 
