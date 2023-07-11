6f40f78ca9fafd3bd40c15c9b688380126dc11de
Andrew Healey <doolie.healey@gmail.com>
2023-07-11 17:30:07 -0400

    Reproduce rough results from persam_f

--- scripts/persam_presets.sh
+++ scripts/persam_presets.sh
@@ -1,18 +1,20 @@
-# Disable globbing
-set -f
 
-if [ "$1" = "backpack_dog" ]
-then
+data_classes=($(ls -d ./data/Annotations/*/))
+if printf '%s\0' "${data_classes[@]}" | grep -F -x -z -- ./data/Annotations/$1/; then
 args=$(cat <<-END
-    --ref_img ./data/Images/backpack_dog/00.jpg
-    --ref_mask ./data/Annotations/backpack_dog/00.png
-    --img_dir ./data/Images/backpack_dog
+    --ref_img ./data/Images/$1/00.jpg
+    --ref_mask ./data/Annotations/$1/00.png
+    --img_dir ./data/Images/$1
     --out_dir ./output
 END
 )
 fi
 if [ "$1" = "perseg" ]
 then
+
+# Disable globbing
+set -f
+
 args=$(cat <<-END
     --ref_img ./data/Images/*/00.jpg
     --ref_mask ./data/Annotations/*/00.png
@@ -26,4 +28,6 @@ script_name=${2:-persam/persam.py}
 new_args=$(echo "$script_name" $args "${@:3}")
 echo $new_args
 python $new_args
+
+# Re-enable globbing
 set +f
