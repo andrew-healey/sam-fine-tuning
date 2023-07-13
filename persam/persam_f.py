760731ef75cef8d13f42f9f3c10bb5af5d8652d4
Andrew Healey <doolie.healey@gmail.com>
2023-07-13 18:25:42 -0400

    Propagate change

--- persam/persam_f.py
+++ persam/persam_f.py
@@ -489,14 +489,14 @@ if __name__ == "__main__":
     rmrf(args.out_dir)
     mkdirp(args.out_dir)
 
-    for ref_img_path, ref_mask_path, test_img_dir, output_dir in load_dirs(
-        args.ref_img, args.ref_mask, args.img_dir, args.out_dir
+    for ref_img_dir, ref_mask_dir, test_img_dir, output_dir in load_dirs(
+        args.ref_img_dir, args.ref_mask_dir, args.img_dir, args.out_dir
     ):
         print(f"Processing {test_img_dir}...")
         persam_f(
             predictor,
-            ref_img_path,
-            ref_mask_path,
+            ref_img_dir,
+            ref_mask_dir,
             test_img_dir,
             output_dir,
             experiment_name,
