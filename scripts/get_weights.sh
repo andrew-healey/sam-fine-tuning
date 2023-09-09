399546387b1b2c5fa1bf36392134f338001cbe02
Andrew Healey <doolie.healey@gmail.com>
2023-09-09 02:58:24 +0000

    Make AWS Batch infer work

--- scripts/get_weights.sh
+++ scripts/get_weights.sh
@@ -1,3 +1,4 @@
+mkdir weights/
 cd weights/
 wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
 wget https://github.com/ChaoningZhang/MobileSAM/raw/master/weights/mobile_sam.pt
