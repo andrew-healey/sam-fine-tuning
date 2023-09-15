19e1057a1c44087ea3417c8ba27e284afab95a12
Andrew Healey <doolie.healey@gmail.com>
2023-09-15 09:12:37 +0000

    Change experiments for new overnight run

--- experiments_scripts/sep_11.sh
+++ experiments_scripts/sep_11.sh
@@ -9,22 +9,25 @@ export MAX_IDLE_TIME_SECONDS=3600 # 1 hr of training
 # python -m fine_tune.aws_smart_poly_config --name lora_8 --use_decoder_lora --decoder_lora_r=8 --group lora
 # python -m fine_tune.aws_smart_poly_config --name lora_0 --group lora
 
-# python -m fine_tune.aws_smart_poly_config --name custom_hypers --group hypers
-# python -m fine_tune.aws_smart_poly_config --name no_custom_hypers --group hypers --no_custom_hypers
+python -m fine_tune.aws_smart_poly_config --name custom_hypers --group hypers
+python -m fine_tune.aws_smart_poly_config --name no_custom_hypers --group hypers --no_custom_hypers
 
 # python -m fine_tune.aws_smart_poly_config --name inf_prompts --group num_train_prompts
 # python -m fine_tune.aws_smart_poly_config --name 5k_prompts --train_prompts=5000 --group num_train_prompts
 # python -m fine_tune.aws_smart_poly_config --name 4k_prompts --train_prompts=4000 --group num_train_prompts
 # python -m fine_tune.aws_smart_poly_config --name 3k_prompts --train_prompts=3000 --group num_train_prompts
-python -m fine_tune.aws_smart_poly_config --name 2k_prompts --train_prompts=2000 --group num_train_prompts
-python -m fine_tune.aws_smart_poly_config --name 1k_prompts --train_prompts=1000 --group num_train_prompts
+# python -m fine_tune.aws_smart_poly_config --name 2k_prompts --train_prompts=2000 --group num_train_prompts
+# python -m fine_tune.aws_smart_poly_config --name 1k_prompts --train_prompts=1000 --group num_train_prompts
 
-python -m fine_tune.aws_smart_poly_config --name 2_lr --initial_lr=0.0002 --group lrs
-python -m fine_tune.aws_smart_poly_config --name 4_lr --initial_lr=0.0004 --group lrs
-python -m fine_tune.aws_smart_poly_config --name 8_lr --initial_lr=0.0008 --group lrs
+# python -m fine_tune.aws_smart_poly_config --name 2_lr --initial_lr=0.0002 --group lrs
+# python -m fine_tune.aws_smart_poly_config --name 4_lr --initial_lr=0.0004 --group lrs
+# python -m fine_tune.aws_smart_poly_config --name 8_lr --initial_lr=0.0008 --group lrs
 
-python -m fine_tune.aws_smart_poly_config --name embed --use_patch_embed --group patch_embed
-python -m fine_tune.aws_smart_poly_config --name no_embed --group patch_embed
+# python -m fine_tune.aws_smart_poly_config --name embed --use_patch_embed --group patch_embed
+# python -m fine_tune.aws_smart_poly_config --name no_embed --group patch_embed
+
+python -m fine_tune.aws_smart_poly_config --name sam_hq_loss --sam_hq_loss --group sam_hq_loss
+python -m fine_tune.aws_smart_poly_config --name normal_loss --group sam_hq_loss
 
 python -m fine_tune.aws_smart_poly_config --name 0_refine --num_refinement_steps=0 --group clicks
 python -m fine_tune.aws_smart_poly_config --name 1_refine --num_refinement_steps=1 --group clicks
@@ -39,9 +42,6 @@ python -m fine_tune.aws_smart_poly_config --name 5_batch --batch_size=5 --group 
 python -m fine_tune.aws_smart_poly_config --name 20_batch --batch_size=20 --group batch_size
 python -m fine_tune.aws_smart_poly_config --name 50_batch --batch_size=50 --group batch_size
 python -m fine_tune.aws_smart_poly_config --name 100_batch --batch_size=100 --group batch_size
-
-python -m fine_tune.aws_smart_poly_config --name sam_hq_loss --sam_hq_loss --group sam_hq_loss
-python -m fine_tune.aws_smart_poly_config --name normal_loss --group sam_hq_loss
 
 python -m fine_tune.aws_smart_poly_config --name warm --group warm_start
 python -m fine_tune.aws_smart_poly_config --name cold --no_warm_start --group warm_start
